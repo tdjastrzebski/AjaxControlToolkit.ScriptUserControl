f85e1603a4977b423623932037a685a92d0936a4
tdjastrzebski <tdjastrzebski@wp.pl>
2018-04-20 00:55:31 +0200

    first version

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ASP.NET AJAX Control Toolkit - ScriptUserControl")]
+[assembly: AssemblyProduct("ASP.NET AJAX Control Toolkit - ScriptUserControl")]
+[assembly: AssemblyDescription("ScriptUserControl extension to AJAX Control Toolkit")]
+[assembly: AssemblyCompany("CodePlex Foundation")]
+[assembly: AssemblyCopyright("Copyright © CodePlex Foundation 2012-2018")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("426bffec-ddd8-442b-9850-9b9aa5808b9a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
